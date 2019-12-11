14a1e2a9a8d992cf5f898d8def59635480ed647b
Johnliveira <jonhjonholiveira@gmail.com>
2019-12-11 17:54:44 -0300

    Simulando cliente

---
+++ src/br/com/caelum/chat/Cliente.java
@@ -0,0 +1,26 @@
+package br.com.caelum.chat;
+
+import java.io.DataOutputStream;
+import java.io.FilterOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import br.com.caelum.utils.Separador;
+
+public class Cliente {
+
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		
+		Socket cliente = new Socket("127.0.0.1", 12345);
+		System.out.println("O cliente conectou ao servidor!");
+		
+		FilterOutputStream saida = new DataOutputStream(cliente.getOutputStream());
+		Separador separador = new Separador();
+		
+		saida.write(separador.carrega());
+		
+		saida.close();
+		cliente.close();
+	}
+}
